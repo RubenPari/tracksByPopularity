2a3d76e58a90089423464ecb6028fb61358bb26f
RubenPari <rubenpari.developer@gmail.com>
2024-05-31 17:18:36 +0200

    added 2 methods with PlaylistHelper.cs

---
+++ helpers/PlaylistHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace tracksByPopularity.helpers;
+
+public static class PlaylistHelper
+{
+    public static async Task<bool> CheckValidityPlaylist(params string[] playlistIds)
+    {
+        foreach (var playlistId in playlistIds)
+        {
+            var playlist = await Client.Spotify.Playlists.Get(playlistId);
+            if (playlist.Id is null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static async Task<bool> CheckIsEmptyPlaylist(params string[] playlistIds)
+    {
+        foreach (var playlistId in playlistIds)
+        {
+            var playlist = await Client.Spotify.Playlists.Get(playlistId);
+            if (playlist.Tracks!.Items!.Count > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
