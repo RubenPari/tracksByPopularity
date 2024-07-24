3cee14cea750a91c8c7ca42609215e4807ddef95
RubenPari <rubenpari.developer@gmail.com>
2024-07-24 23:58:16 +0200

    added QueryParamHelper.cs

---
+++ helpers/QueryParamHelper.cs
@@ -0,0 +1,24 @@
+using tracksByPopularity.models;
+
+namespace tracksByPopularity.helpers;
+
+public static class QueryParamHelper
+{
+    public static TimeRangeEnum GetTimeRangeQueryParam(HttpContext context)
+    {
+        var timeRange = context.Request.Query["timeRange"].FirstOrDefault();
+
+        if (string.IsNullOrEmpty(timeRange))
+        {
+            return TimeRangeEnum.NotValid;
+        }
+
+        // convert timeRange from string to enum
+        if (!Enum.TryParse<TimeRangeEnum>(timeRange, out var timeRangeEnum))
+        {
+            return TimeRangeEnum.NotValid;
+        }
+        
+        return timeRangeEnum;
+    }
+}
