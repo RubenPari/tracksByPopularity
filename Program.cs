4145579c48ee51932a2c311799290efa4df70a6f
RubenPari <rubenpari.developer@gmail.com>
2024-07-28 00:28:25 +0200

    added routes mapping

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@ using dotenv.net;
 using StackExchange.Redis;
 using tracksByPopularity;
 using tracksByPopularity.background;
-using tracksByPopularity.controllers;
 using tracksByPopularity.middlewares;
+using tracksByPopularity.routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,32 +46,6 @@ app.UseSwaggerUi(config =>
 
 DotEnv.Load();
 
-// map controllers
-
-// ####### /AUTH #######
-
-var authRoutes = app.MapGroup("/auth");
-
-authRoutes.MapGet("/login", AuthController.Login);
-
-authRoutes.MapGet("/callback", AuthController.Callback);
-
-authRoutes.MapGet("/logout", AuthController.Logout);
-
-// ####### /TRACK #######
-
-var trackRoutes = app.MapGroup("/track");
-
-trackRoutes.MapPost("/top", TrackController.Top);
-
-trackRoutes.MapPost("/less", TrackController.Less);
-
-trackRoutes.MapPost("/less-medium", TrackController.LessMedium);
-
-trackRoutes.MapPost("/more-medium", TrackController.MoreMedium);
-
-trackRoutes.MapPost("/more", TrackController.More);
-
-trackRoutes.MapPost("/artist", TrackController.Artist);
+Routes.MapRoutes(app);
 
 app.Run();
