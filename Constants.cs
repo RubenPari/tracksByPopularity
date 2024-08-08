02ee17575f60e65833cc16b121d71997ccf32d5e
RubenPari <rubenpari.developer@gmail.com>
2024-08-08 12:40:29 +0200

    formatting

--- Constants.cs
+++ Constants.cs
@@ -13,15 +13,15 @@ namespace tracksByPopularity
         public static int TracksMoreMediumPopularity => 75;
 
         public static List<string> MyScopes { get; } =
-        [
-            Scopes.UserReadEmail,
-            Scopes.UserReadPrivate,
-            Scopes.UserLibraryRead,
-            Scopes.UserLibraryModify,
-            Scopes.UserTopRead,
-            Scopes.PlaylistModifyPrivate,
-            Scopes.PlaylistModifyPublic
-        ];
+            [
+                Scopes.UserReadEmail,
+                Scopes.UserReadPrivate,
+                Scopes.UserLibraryRead,
+                Scopes.UserLibraryModify,
+                Scopes.UserTopRead,
+                Scopes.PlaylistModifyPrivate,
+                Scopes.PlaylistModifyPublic
+            ];
 
         public static string ClientId { get; } = Environment.GetEnvironmentVariable("CLIENT_ID")!;
 
