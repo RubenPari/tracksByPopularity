175daba7ce7085b61fc0c1ccff7e663acf6aecac
RubenPari <rubenpari.developer@gmail.com>
2024-08-08 12:35:31 +0200

    cleared code

--- services/PlaylistService.cs
+++ services/PlaylistService.cs
@@ -7,12 +7,11 @@ public static class PlaylistService
 {
     public static async Task<RemoveAllTracksResponse> RemoveAllTracks(string playlistId)
     {
-        const string baseUrl = "https://clear-songs.fly.dev";
         var client = new HttpClient();
         var request = new HttpRequestMessage
         {
             Method = HttpMethod.Get,
-            RequestUri = new Uri($"{baseUrl}/playlist/delete-tracks?id_playlist={playlistId}"),
+            RequestUri = new Uri($"{Constants.ClearSongsBaseUrl}/playlist/delete-tracks?id_playlist={playlistId}"),
         };
 
         using var response = await client.SendAsync(request);
