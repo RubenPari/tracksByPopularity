4caefeb546c698e975653b24b111fd7ed1f80b18
RubenPari <rubenpari.developer@gmail.com>
2025-02-27 19:37:31 +0100

    changed CheckValidityPlaylist with GetOrCreateArtistPlaylists

--- src/helpers/PlaylistHelper.cs
+++ src/helpers/PlaylistHelper.cs
@@ -1,20 +1,66 @@
-﻿using tracksByPopularity.utils;
+﻿using SpotifyAPI.Web;
+using tracksByPopularity.utils;
 
 namespace tracksByPopularity.helpers;
 
 public static class PlaylistHelper
 {
-    public static async Task<bool> CheckValidityPlaylist(params string[] playlistIds)
+    public static async Task<Dictionary<string, string>> GetOrCreateArtistPlaylists(string artistId)
     {
-        foreach (var playlistId in playlistIds)
+        var artistPlaylistsId = new Dictionary<string, string>();
+        var userId = (await Client.Spotify!.UserProfile.Current()).Id;
+        var artistName = (await Client.Spotify.Artists.Get(artistId)).Name;
+
+        // Recupera tutte le playlist dell'utente
+        var pagingUserPlaylists = await Client.Spotify.Playlists.GetUsers(userId);
+        var userPlaylists = await Client.Spotify.PaginateAll(pagingUserPlaylists);
+
+        // Cerca se esistono già le playlist dell'artista (less-medium-more)
+        foreach (var userPlaylist in userPlaylists)
         {
-            var playlist = await Client.Spotify!.Playlists.Get(playlistId);
-            if (playlist.Id is null)
+            if (userPlaylist.Name == $"{artistName} less")
             {
-                return false;
+                artistPlaylistsId["less"] = userPlaylist.Id!;
+            }
+            else if (userPlaylist.Name == $"{artistName} medium")
+            {
+                artistPlaylistsId["medium"] = userPlaylist.Id!;
+            }
+            else if (userPlaylist.Name == $"{artistName} more")
+            {
+                artistPlaylistsId["more"] = userPlaylist.Id!;
             }
         }
 
-        return true;
+        if (artistPlaylistsId.Count == 3)
+        {
+            return artistPlaylistsId;
+        }
+
+        // Crea le playlist se non esistono già
+        artistPlaylistsId.Clear();
+
+        artistPlaylistsId["less"] = (
+            await Client.Spotify.Playlists.Create(
+                userId,
+                new PlaylistCreateRequest($"{artistName} less")
+            )
+        ).Id!;
+
+        artistPlaylistsId["medium"] = (
+            await Client.Spotify.Playlists.Create(
+                userId,
+                new PlaylistCreateRequest($"{artistName} medium")
+            )
+        ).Id!;
+
+        artistPlaylistsId["more"] = (
+            await Client.Spotify.Playlists.Create(
+                userId,
+                new PlaylistCreateRequest($"{artistName} more")
+            )
+        ).Id!;
+
+        return artistPlaylistsId;
     }
 }
