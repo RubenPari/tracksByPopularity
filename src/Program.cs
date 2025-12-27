676fb5f501b2592eab9db838fb82679294d3aea9
RubenPari <rubenpari.developer@gmail.com>
2025-12-28 00:30:51 +0100

    refactor(backend): configure FluentValidation and IOptions in Program.cs
    - Configure FluentValidation for automatic request validation
    - Register ConfigurationService and domain services
    - Improve service registration and dependency injection setup

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,30 @@
 using dotenv.net;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using StackExchange.Redis;
 using tracksByPopularity.background;
 using tracksByPopularity.middlewares;
 using tracksByPopularity.routes;
 using tracksByPopularity.services;
 using tracksByPopularity.utils;
+using tracksByPopularity.validators;
+using tracksByPopularity.configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 DotEnv.Load();
 
-// Add controllers
-builder.Services.AddControllers();
+// Configure application settings
+builder.Services.AddApplicationConfiguration(builder.Configuration);
+
+// Add controllers with FluentValidation
+builder.Services.AddControllers()
+    .AddFluentValidation(fv =>
+    {
+        fv.RegisterValidatorsFromAssemblyContaining<AddTracksByArtistRequestValidator>();
+        fv.AutomaticValidationEnabled = true;
+        fv.ImplicitlyValidateChildProperties = true;
+    });
 
 // Service that set Redis cache
 builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
@@ -40,6 +53,8 @@ builder.Services.AddScoped<IPlaylistService, PlaylistService>();
 builder.Services.AddScoped<IArtistService, ArtistService>();
 builder.Services.AddScoped<ICacheService, CacheService>();
 builder.Services.AddScoped<IPlaylistHelper, PlaylistHelperService>();
+builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
+builder.Services.AddScoped<domain.services.ITrackCategorizationService, domain.services.TrackCategorizationService>();
 
 builder.Services.AddHttpClient();
 
