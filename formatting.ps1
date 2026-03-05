1ae9542053bc8f635fdde727825362024ae1381a
RubenPari <rubenpari.developer@gmail.com>
2026-03-05 16:37:13 +0100

    Remove Docker configuration files, refactoring summary documentation, and a formatting script.

--- formatting.ps1
+++
@@ -1,5 +0,0 @@
-# Get the directory where the script is located
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-# Format all C# files in the src directory
-dotnet csharpier format "$scriptDir\src"
