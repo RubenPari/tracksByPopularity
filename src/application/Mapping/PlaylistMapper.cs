182c67b386cda0ba32ef676f91222f0e26d9b5a4
RubenPari <rubenpari.developer@gmail.com>
2026-03-25 03:46:39 +0100

    Fix PlaylistMapper MapProperty attribute for Tracks.Total mapping

--- src/application/Mapping/PlaylistMapper.cs
+++ src/application/Mapping/PlaylistMapper.cs
@@ -7,6 +7,6 @@ namespace tracksByPopularity.Application.Mapping;
 [Mapper]
 public partial class PlaylistMapper
 {
-    [MapProperty("Tracks.Total", "TotalTracks")]
+    [MapProperty(nameof(FullPlaylist.Tracks) + "." + nameof(Paging<FullTrack>.Total), nameof(PlaylistInfo.TotalTracks))]
     public partial PlaylistInfo MapToPlaylistInfo(FullPlaylist playlist);
 }
